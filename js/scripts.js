6861ea11330a6872ce739669b411e3a016a6a09e
Faisal Rana <inquisitive@gmail.com>
2021-01-30 18:13:08 -0800

    everything is working, but cannot resubmit form

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,36 @@ $(document).ready(function() {
     const question4 = $('#question4').val();
     const question5 = $('#question5').val();
     if(question1 === "1") {
-      $("#hiddenoutput").css("color", "red");
-      $("#html").css("color","blue");
+      $("#swift").css("color","yellow");
+      $("#objc").css("color","yellow");
+      $("#applescript").css("color","yellow");
+      $("#c").css("color","yellow");
     }
+    if(question2 === "1") {
+      $("#typescript").css("color","yellow");
+      $("#unity").css("color","yellow");
+    }
+    if(question3 === "1") {
+      $("#f").css("color","yellow");
+      $("#csharp").css("color","yellow");
+      $("#visualbasic").css("color","yellow");
+      $("#html").css("color","yellow");
+      $("#flutter").css("color","yellow");
+    }
+    if(question4 === "1") {
+      $("#java").css("color","yellow");
+      $("#k").css("color","yellow");
+      $("#cplusplus").css("color","yellow");
+      $("#python").css("color","yellow");
+      $("#html").css("color","yellow");
+    }
+    if(question5 === "1") {
+      $("#python").css("color","yellow");
+      $("#lisp").css("color","yellow");
+      $("#prolog").css("color","yellow");
+    }
+    
+
     $("#hiddenoutput").show();
     event.preventDefault();
   });
